cad94fed6ee5eeb6759baf6f60fababd260d287e
Ubuntu <ubuntu@ip-172-31-30-83.ec2.internal>
2024-12-17 23:51:57 +0000

    Updated Syntax

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var host;
 var handleRequest = function(request, response) {
   response.setHeader('Content-Type', 'text/plain');
   response.writeHead(200);
-  response.write("DevOps Coursework 2! | Running on: ");
+  response.write("DevOps Coursework 2! | Running on : ");
   response.write(host);
   response.end(" | v=0\n");
   console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
